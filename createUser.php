40036cb295b8d45b0ffecc8cb7b10b4ca94e6b3c
jimmykahalpara <jimmyatridhyatech@gmail.com>
2022-02-14 19:01:50 +0530

    bux fix

--- createUser.php
+++ createUser.php
@@ -3,7 +3,6 @@ require_once "Utilities/helpers.php";
 session_start();
 
 if (isset($_POST['submit'])) {
-
 }
 
 
@@ -18,18 +17,19 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create New Users - <?php echo $title ?></title>
     <link rel="stylesheet" href="Styles/loginStyle.css">
-    <?php importBootstrapCSS(); setFont(); ?>
+    <?php importBootstrapCSS();
+    setFont(); ?>
 
 
 </head>
 
 <body>
     <div class="mainContainer d-flex justify-content-center align-items-center p-5">
-        <div class="loginContainer w-75">
+        <div class="loginContainer w-75 p-5">
             <form action="#" method="post" class="" id="createUserForm" novalidate>
                 <div class="form-group">
                     <label for="input_first_name">
-                        First Name:
+                        First Name
                     </label>
                     <input type="text" class="form-control" name="first_name" id="input_first_name" required oninvalid="this.setCustomValidity('First Name Required')" oninput="this.setCustomValidity('')">
                     <div class="valid-feedback">
@@ -77,7 +77,7 @@ if (isset($_POST['submit'])) {
                 </div>
                 <div class="form-group">
                     <label for="input_password_2">
-                        Re-Enter Password:
+                        Re-Enter Password
                     </label>
                     <input type="password" name="password_2" id="input_password_2" class="form-control">
                     <div class="valid-feedback">
@@ -102,7 +102,7 @@ if (isset($_POST['submit'])) {
 
                 <div class="form-group">
                     <label for="input_gender">
-                        Select Gender: 
+                        Select Gender:
                     </label>
                     <select name="gender" id="input_gender" class="custom-select">
                         <option value="0" selected>Select Gender</option>
@@ -143,9 +143,10 @@ if (isset($_POST['submit'])) {
                     </div>
                 </div>
 
+                <div class="submitButtonContainer d-flex justify-content-center align-items-center">
+                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </div>
 
-
-                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 
             </form>
         </div>
@@ -155,152 +156,169 @@ if (isset($_POST['submit'])) {
     <?php importBootstrapJS() ?>
 
     <script>
-        function validateForm(){
-                firstName = $('#input_first_name');
-                lastName = $('#input_last_name');
-                email = $('#input_email');
-                password_1 = $('#input_password_1');
-                password_2 = $('#input_password_2');
-                phone = $('#input_phone');
-                gender = $('#input_gender');
-                dob = $("#input_dob");
-                profile_pic = $('#input_profile');
+        function validateForm() {
+            formValid = true;
+            firstName = $('#input_first_name');
+            lastName = $('#input_last_name');
+            email = $('#input_email');
+            password_1 = $('#input_password_1');
+            password_2 = $('#input_password_2');
+            phone = $('#input_phone');
+            gender = $('#input_gender');
+            dob = $("#input_dob");
+            profile_pic = $('#input_profile');
 
-                firstNameValue = firstName.val();
-                lastNameValue = lastName.val();
-                emailValue = email.val();
-                password_1Value = password_1.val();
-                password_2Value = password_2.val();
-                phoneValue = phone.val();
-                genderValue = gender.val();
-                dobValue = dob.val();
-                picValue = profile_pic.val()
+            firstNameValue = firstName.val();
+            lastNameValue = lastName.val();
+            emailValue = email.val();
+            password_1Value = password_1.val();
+            password_2Value = password_2.val();
+            phoneValue = phone.val();
+            genderValue = gender.val();
+            dobValue = dob.val();
+            picValue = profile_pic.val()
 
-                if (firstNameValue.length <= 0){
-                    firstName.removeClass(['is-valid', 'is-invalid']);
-                    firstName.addClass('is-invalid');
-                } else {
-                    firstName.removeClass(['is-valid', 'is-invalid']);
-                    firstName.addClass('is-valid');
-                }
+            if (firstNameValue.length <= 0) {
+                firstName.removeClass(['is-valid', 'is-invalid']);
+                firstName.addClass('is-invalid');
+                formValid = false;
+            } else {
+                firstName.removeClass(['is-valid', 'is-invalid']);
+                firstName.addClass('is-valid');
+            }
 
-                if (lastNameValue.length <= 0){
-                    lastName.removeClass(['is-valid', 'is-invalid']);
-                    lastName.addClass('is-invalid');
-                } else {
-                    lastName.removeClass(['is-valid', 'is-invalid']);
-                    lastName.addClass('is-valid');
-                }
+            if (lastNameValue.length <= 0) {
+                lastName.removeClass(['is-valid', 'is-invalid']);
+                lastName.addClass('is-invalid');
+                formValid = false;
+            } else {
+                lastName.removeClass(['is-valid', 'is-invalid']);
+                lastName.addClass('is-valid');
+            }
 
-                var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]+)+$/;
-                if(!regex.test(emailValue)){
-                    email.removeClass(['is-valid', 'is-invalid']);
-                    email.addClass('is-invalid');
-                } else {
-                    email.removeClass(['is-valid', 'is-invalid']);
-                    email.addClass('is-valid');
-                }
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]+)+$/;
+            if (!regex.test(emailValue)) {
+                email.removeClass(['is-valid', 'is-invalid']);
+                email.addClass('is-invalid');
+                formValid = false;
+            } else {
+                email.removeClass(['is-valid', 'is-invalid']);
+                email.addClass('is-valid');
+            }
 
-                regexNumber = /^.*\d.*$/;
-                regexUpperCaseLetter = /^.*[A-Z].*$/
-                regexLowerCaseLetter = /^.*[a-z].*$/
-                regexSpecialCharacters = /^.*[\~\`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\|\\\'\"\;\:\?\/\>\.\<\,].*$/;
+            regexNumber = /^.*\d.*$/;
+            regexUpperCaseLetter = /^.*[A-Z].*$/
+            regexLowerCaseLetter = /^.*[a-z].*$/
+            regexSpecialCharacters = /^.*[\~\`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\|\\\'\"\;\:\?\/\>\.\<\,].*$/;
 
-                console.log(password_1Value.length, "this is great");
-                if (password_1Value.length <= 7) {
-                    $('#invalid_feedback_password1').text('Your password should contain atleast 8 characters, atleast one number, one upper case letter,one lower case letter, and one special character');
-                    password_1.removeClass(['is-valid', 'is-invalid']);
-                    password_1.addClass('is-invalid');
-                } else if (!regexNumber.test(password_1Value)){
-                    $('#invalid_feedback_password1').text('Should contain at least one number!');
-                    password_1.removeClass(['is-valid', 'is-invalid']);
-                    password_1.addClass('is-invalid');
-                } else if (!regexUpperCaseLetter.test(password_1Value)){
-                    $('#invalid_feedback_password1').text('Should contain at least one Upper Case Letter!');
-                    password_1.removeClass(['is-valid', 'is-invalid']);
-                    password_1.addClass('is-invalid');
-                } else if (!regexLowerCaseLetter.test(password_1Value)){
-                    $('#invalid_feedback_password1').text('Should contain at least one Lower Case Letter!');
-                    password_1.removeClass(['is-valid', 'is-invalid']);
-                    password_1.addClass('is-invalid');
-                } else if (!regexSpecialCharacters.test(password_1Value)){
-                    $('#invalid_feedback_password1').text('Should contain at least one Special Character');
-                    password_1.removeClass(['is-valid', 'is-invalid']);
-                    password_1.addClass('is-invalid');
-                } else {
-                    password_1.removeClass(['is-valid', 'is-invalid']);
-                    password_1.addClass('is-valid');
-                }
-
-
-                if (password_1Value != password_2Value){
-                    password_2.removeClass(['is-valid', 'is-invalid']);
-                    password_2.addClass('is-invalid');
-                } else {
-                    password_2.removeClass(['is-valid', 'is-invalid']);
-                    password_2.addClass('is-valid');
-                }
-
-
-                phoneRegex = /^\d{10}$/;
-                if (!phoneRegex.test(phoneValue)){
-                    phone.removeClass(['is-valid', 'is-invalid']);
-                    phone.addClass('is-invalid');
-                } else {
-                    phone.removeClass(['is-valid', 'is-invalid']);
-                    phone.addClass('is-valid');
-                }
-
-                if (genderValue == 0){
-                    gender.removeClass(['is-valid', 'is-invalid']);
-                    gender.addClass('is-invalid');
-                } else {
-                    gender.removeClass(['is-valid', 'is-invalid']);
-                    gender.addClass('is-valid');
-                }
-
-                currDate = new Date();
-                dobDate = new Date(dobValue);
-                if (dobDate >= currDate){
-                    dob.removeClass(['is-valid', 'is-invalid']);
-                    dob.addClass('is-invalid');
-                } else {
-                    dob.removeClass(['is-valid', 'is-invalid']);
-                    dob.addClass('is-valid');
-                }
-
-                if (picValue.length <= 0){
-                    profile_pic.removeClass(['is-valid', 'is-invalid']);
-                    profile_pic.addClass('is-invalid');
-                } else {
-                    profile_pic.removeClass(['is-valid', 'is-invalid']);
-                    profile_pic.addClass('is-valid');
-                }
+            console.log(password_1Value.length, "this is great");
+            if (password_1Value.length <= 7) {
+                $('#invalid_feedback_password1').text('Your password should contain atleast 8 characters, atleast one number, one upper case letter,one lower case letter, and one special character');
+                password_1.removeClass(['is-valid', 'is-invalid']);
+                password_1.addClass('is-invalid');
+                formValid = false;
+            } else if (!regexNumber.test(password_1Value)) {
+                $('#invalid_feedback_password1').text('Should contain at least one number!');
+                password_1.removeClass(['is-valid', 'is-invalid']);
+                formValid = false;
+                password_1.addClass('is-invalid');
+            } else if (!regexUpperCaseLetter.test(password_1Value)) {
+                $('#invalid_feedback_password1').text('Should contain at least one Upper Case Letter!');
+                password_1.removeClass(['is-valid', 'is-invalid']);
+                formValid = false;
+                password_1.addClass('is-invalid');
+            } else if (!regexLowerCaseLetter.test(password_1Value)) {
+                $('#invalid_feedback_password1').text('Should contain at least one Lower Case Letter!');
+                password_1.removeClass(['is-valid', 'is-invalid']);
+                password_1.addClass('is-invalid');
+                formValid = false;
+            } else if (!regexSpecialCharacters.test(password_1Value)) {
+                $('#invalid_feedback_password1').text('Should contain at least one Special Character');
+                password_1.removeClass(['is-valid', 'is-invalid']);
+                password_1.addClass('is-invalid');
+                formValid = false;
+            } else {
+                password_1.removeClass(['is-valid', 'is-invalid']);
+                password_1.addClass('is-valid');
+            }
 
 
-                return true;
+            if (password_1Value != password_2Value) {
+                password_2.removeClass(['is-valid', 'is-invalid']);
+                password_2.addClass('is-invalid');
+                formValid = false;
+            } else {
+                password_2.removeClass(['is-valid', 'is-invalid']);
+                password_2.addClass('is-valid');
+            }
 
-                
-        }       
 
-        $(document).ready(function(){
-            $("#createUserForm").submit(function(e){
-                
-                e.preventDefault();
+            phoneRegex = /^\d{10}$/;
+            if (!phoneRegex.test(phoneValue)) {
+                phone.removeClass(['is-valid', 'is-invalid']);
+                phone.addClass('is-invalid');
+                formValid = false;
+            } else {
+                phone.removeClass(['is-valid', 'is-invalid']);
+                phone.addClass('is-valid');
+            }
+
+            if (genderValue == 0) {
+                gender.removeClass(['is-valid', 'is-invalid']);
+                gender.addClass('is-invalid');
+                formValid = false;
+            } else {
+                gender.removeClass(['is-valid', 'is-invalid']);
+                gender.addClass('is-valid');
+            }
+
+            currDate = new Date();
+            dobDate = new Date(dobValue);
+            if (dobDate >= currDate || dobValue == '') {
+                dob.removeClass(['is-valid', 'is-invalid']);
+                dob.addClass('is-invalid');
+                formValid = false;
+            } else {
+                dob.removeClass(['is-valid', 'is-invalid']);
+                dob.addClass('is-valid');
+            }
+
+            if (picValue.length <= 0) {
+                profile_pic.removeClass(['is-valid', 'is-invalid']);
+                profile_pic.addClass('is-invalid');
+                formValid = false;
+
+            } else {
+                profile_pic.removeClass(['is-valid', 'is-invalid']);
+                profile_pic.addClass('is-valid');
+
+            }
+
+
+            return formValid;
+
+
+        }
+
+        $(document).ready(function() {
+            $("#createUserForm").submit(function(e) {
+
+                // e.preventDefault();
                 $('input').on('keyup', validateForm);
                 $('#input_gender').on('change', validateForm);
-                
-                
+                if (!validateForm()) {
+                    e.preventDefault();
+                }
+
+                $("#input_dob").datepicker().on('changeDate', validateForm);
+
 
             });
 
 
 
-            
+
         });
-
-        
-
     </script>
 </body>
 
