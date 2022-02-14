37eaffc5fcd4f0629338eb4a72b13a90c643a233
jimmyahalpara <jimmyahalpara123@gmail.com>
2022-02-15 01:07:40 +0530

    Update: Complete jquery validation for all createUser fields

--- createUser.php
+++ createUser.php
@@ -75,7 +75,7 @@ if (isset($_POST['submit'])) {
                         Select Gender:
                     </label>
                     <select name="gender" id="input_gender" class="custom-select">
-                        <option value="0" selected>Select Gender</option>
+                        <option value="" selected>Select Gender</option>
                         <option value="1">Female</option>
                         <option value="2">Male</option>
                     </select>
@@ -111,6 +111,40 @@ if (isset($_POST['submit'])) {
     <?php importBootstrapJS() ?>
 
     <script>
+        jQuery.validator.addMethod('validatePassword', function(value, element, params) {
+            password_1Value = element.value;
+            regexNumber = /^.*\d.*$/;
+            regexUpperCaseLetter = /^.*[A-Z].*$/
+            regexLowerCaseLetter = /^.*[a-z].*$/
+            regexSpecialCharacters = /^.*[\~\`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\|\\\'\"\;\:\?\/\>\.\<\,].*$/;
+
+            if (password_1Value.length <= 7) {
+                return false;
+            } else if (!regexNumber.test(password_1Value)) {
+                return false;
+            } else if (!regexUpperCaseLetter.test(password_1Value)) {
+                return false;
+            } else if (!regexLowerCaseLetter.test(password_1Value)) {
+                return false;
+            } else if (!regexSpecialCharacters.test(password_1Value)) {
+                return false
+            } else {
+                return true;
+            }
+
+        });
+
+        jQuery.validator.addMethod('validateDate', function(value, element, params){
+            dt = new Date(value);
+            current_dt = new Date();
+            if (dt > current_dt) {
+                return false;
+            } else {
+                return true;
+            }
+        });
+
+        
         jQuery('#createUserForm').validate({
             rules: {
                 first_name: {
@@ -123,20 +157,42 @@ if (isset($_POST['submit'])) {
                 },
                 email: "required",
                 password_1: {
-                    required: function(element){
-                        
-                    },
+                    required: true,
+                    validatePassword: true,
                     minlength: 8,
                     maxlength: 128,
 
                 },
+                password_2: {
+                    required: true,
+                    equalTo: "#input_password_1",
+                    minlength: 8,
+                    maxlength: 128,
+                },
+                phone: {
+                    required: true,
+                    minlength: 10,
+                    maxlength: 10,
+                    digits: true
+                },
+                gender: {
+                    required: true
+                },
+                dob: {
+                    required: true,
+                    validateDate: true,
+                },
+                profile_image: {
+                    required: true,
+                    accept: "image/*"
+                }
 
             },
             messages: {
                 first_name: {
                     required: "Please enter your first name",
                     maxlength: "Your first name must be less than or equal to 128 characters"
-                },  
+                },
                 last_name: {
                     required: "Please enter your last name",
                     maxlength: "Your last name must be less than or equal to 128 characters"
@@ -145,8 +201,31 @@ if (isset($_POST['submit'])) {
                 password_1: {
                     required: "Please Enter Password",
                     minlength: "Password must be at least 8 characters long",
-                    maxlength: "Password cannot be more than 128 characters long"
+                    maxlength: "Password cannot be more than 128 characters long",
+                    validatePassword: "Password must contain at least one number, one uppercase letter, one lowercase letter, and one special character"
+                },
+                password_2: {
+                    required: "Please Enter Password",
+                    minlength: "Password must be at least 8 characters long",
+                    maxlength: "Password cannot be more than 128 characters long",
+                    equalTo: "Passwords do not match"
+                },
+                phone: {
+                    required: "Please Enter Phone Number",
+                    minlength: "Phone Number must be at least 10 digits long",
+                    maxlength: "Phone Number cannot be more than 10 digits long",
+                    digits: "Phone Number must be digits only"
+                },
+                gender: "Choose an Option",
+                dob: {
+                    require: "Please Enter Valid date",
+                    validateDate: "Enter data from Past"
+                },
+                profile_image:{
+                    required: "Please Upload Profile Image",
+                    accept: "Please Select an Image File"
                 }
+
             }
         });
     </script>
